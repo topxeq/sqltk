9900c8f994370a883ba0f6719c62644f6673ac87
TopXeQ <topxeq@163.com>
2019-10-24 11:08:55 +0800

    a 0.31

--- sqltk.go
+++ sqltk.go
@@ -171,6 +171,67 @@ func QueryDBNS(dbA *sql.DB, sqlStrA string, argsA ...interface{}) ([][]string, e
 	return resultSet, nil
 }
 
+// QueryDBNSS execute a SQL query and return result set(first row will be the column names), all values will be string type(ensure for some DBs, such as MYSQL with uf8_general_ci encoding), can handle null values, passing parameters is supported as well.
+func QueryDBNSS(dbA *sql.DB, sqlStrA string, argsA ...interface{}) ([][]string, error) {
+	rowsT, errT := dbA.Query(sqlStrA, argsA...)
+
+	if errT != nil {
+		return nil, tk.Errf("failed to run query: %v", errT.Error())
+	}
+
+	defer rowsT.Close()
+
+	resultSet := make([][]string, 0)
+	var rowCountT = 0
+	var columnSetT []string = nil
+
+	for rowsT.Next() {
+		rowCountT++
+
+		if columnSetT == nil {
+			columnSetT, errT = rowsT.Columns()
+			if errT != nil {
+				return nil, tk.Errf("failed to get columns of row %v: %v", rowCountT, errT.Error())
+			}
+
+			resultSet = append(resultSet, columnSetT)
+		}
+
+		columnLenT := len(columnSetT)
+
+		var resultRow = make([]interface{}, columnLenT)
+		var resultRowP = make([]interface{}, columnLenT)
+		var resultRowS = make([]string, columnLenT)
+
+		for k := 0; k < columnLenT; k++ {
+			resultRowP[k] = &(resultRow[k])
+		}
+
+		errT = rowsT.Scan(resultRowP...)
+		if errT != nil {
+			return nil, tk.Errf("failed to scan %v: %v", rowCountT, errT.Error())
+		}
+
+		for k := 0; k < columnLenT; k++ {
+			if resultRow[k] == nil {
+				resultRowS[k] = ""
+				continue
+			}
+
+			resultRowS[k] = tk.Spr("%s", resultRow[k])
+		}
+
+		resultSet = append(resultSet, resultRowS)
+	}
+
+	errT = rowsT.Err()
+	if errT != nil {
+		return nil, tk.Errf("error occured while enumerating the result set: %v", errT.Error())
+	}
+
+	return resultSet, nil
+}
+
 // QueryDBI execute a SQL query and return result set(first row will be the column names), all values will be interface{} type, passing parameters is supported as well.
 func QueryDBI(dbA *sql.DB, sqlStrA string, argsA ...interface{}) ([][]interface{}, error) {
 
